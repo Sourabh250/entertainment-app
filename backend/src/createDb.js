1d8d161a8ca68609197d17300331f8924bd3171d
Sourabh Banik <81009734+Sourabh250@users.noreply.github.com>
2024-09-13 07:25:26 +0900

    Updated createDb Logic

--- backend/src/createDb.js
+++ backend/src/createDb.js
@@ -4,6 +4,7 @@ dotenv.config();
 const mongoose = require("mongoose");
 const movieModel = require("./models/movie");
 const tvSeriesModel = require("./models/tvSeries");
+const userModel = require("./models/user");
 
 const apiKey = process.env.TMDB_API_KEY;
 const dbUrl = process.env.DATABASE_URL;
@@ -19,53 +20,77 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-const fetchAndPopulate = async (url,model) => {
-    try {
-        const response = await axios.get(url);
-        const data = response.data.results;
-        const type = model.modelName === 'Movie' ? 'movie' : 'tv';
-        const detailedItems = await Promise.all(data.map(async item => {
-          const detailUrl = `https://api.themoviedb.org/3/${type}/${item.id}?api_key=${apiKey}&append_to_response=credits`;
-          const detailResponse = await axios.get(detailUrl);
-          const details = detailResponse.data;
-    
-          const itemDetails = {
-            ...item,
-            status: details.status || 'N/A',
-            runtime: details.runtime || 0,
-            genre: details.genres.map((genre) => genre.name) || [],
-            tagline: details.tagline || 'N/A',
-            cast: details.credits.cast.map((cast) => cast.name) || [],
-            homepage: details.homepage || 'N/A',
-            imdb_id: details.imdb_id || 'N/A',
-            last_air_date: details.last_air_date || 'N/A',
-          };
+const getAllBookmarkedIds = async () => {
+  const users = await userModel
+    .find()
+    .select("movieBookmarks tvSeriesBookmarks");
+  const movieBookmarks = new Set();
+  const tvSeriesBookmarks = new Set();
 
-          const isValid = Object.keys(model.schema.obj).every((field) => {
-            // If the field is required and the value is missing, mark the item as invalid
-            if (model.schema.obj[field].required && !itemDetails[field]) {
-              console.log(`Skipping item with id ${item.id} due to missing required field: ${field}`);
-              return false;
-            }
-            return true;
-          });
-  
-          return isValid ? itemDetails : null;
+  users.forEach((user) => {
+    user.movieBookmarks.forEach((item) => movieBookmarks.add(item.toString()));
+    user.tvSeriesBookmarks.forEach((item) =>
+      tvSeriesBookmarks.add(item.toString())
+    );
+  });
 
-        }));
-        const filteredItems = detailedItems.filter(item => item !== null);
-        await model.insertMany(filteredItems);
-        console.log(`Db populated with data to ${model.modelName}`);
-    } catch(error) {
-        console.error(`Error fetching data or populating the database:`, error);
-    }
+  return {
+    movieBookmarks: Array.from(movieBookmarks),
+    tvSeriesBookmarks: Array.from(tvSeriesBookmarks),
+  };
 };
 
-const refreshAll = async() => {
-    await movieModel.deleteMany({});
-    await tvSeriesModel.deleteMany({}); 
-    await fetchAndPopulate(movieUrl, movieModel);
-    await fetchAndPopulate(tvUrl, tvSeriesModel); 
-    await mongoose.disconnect();
-}
+const fetchAndPopulate = async (url, model) => {
+  try {
+    const response = await axios.get(url);
+    const data = response.data.results;
+    const type = model.modelName === "Movie" ? "movie" : "tv";
+    const detailedItems = await Promise.all(
+      data.map(async (item) => {
+        const detailUrl = `https://api.themoviedb.org/3/${type}/${item.id}?api_key=${apiKey}&append_to_response=credits`;
+        const detailResponse = await axios.get(detailUrl);
+        const details = detailResponse.data;
+
+        const itemDetails = {
+          ...item,
+          status: details.status || "N/A",
+          runtime: details.runtime || 0,
+          genre: details.genres.map((genre) => genre.name) || [],
+          tagline: details.tagline || "N/A",
+          cast: details.credits.cast.map((cast) => cast.name) || [],
+          homepage: details.homepage || "N/A",
+          imdb_id: details.imdb_id || "N/A",
+          last_air_date: details.last_air_date || "N/A",
+        };
+
+        const isValid = Object.keys(model.schema.obj).every((field) => {
+          // If the field is required and the value is missing, mark the item as invalid
+          if (model.schema.obj[field].required && !itemDetails[field]) {
+            console.log(
+              `Skipping item with id ${item.id} due to missing required field: ${field}`
+            );
+            return false;
+          }
+          return true;
+        });
+
+        return isValid ? itemDetails : null;
+      })
+    );
+    const filteredItems = detailedItems.filter((item) => item !== null);
+    await model.insertMany(filteredItems);
+    console.log(`Db populated with data to ${model.modelName}`);
+  } catch (error) {
+    console.error(`Error fetching data or populating the database:`, error);
+  }
+};
+
+const refreshAll = async () => {
+  const { movieBookmarks, tvSeriesBookmarks } = await getAllBookmarkedIds();
+  await movieModel.deleteMany({ _id: { $nin: movieBookmarks } });
+  await tvSeriesModel.deleteMany({ _id: { $nin: tvSeriesBookmarks } });
+  await fetchAndPopulate(movieUrl, movieModel);
+  await fetchAndPopulate(tvUrl, tvSeriesModel);
+  await mongoose.disconnect();
+};
 refreshAll();
