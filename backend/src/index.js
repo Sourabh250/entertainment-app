51f4a559fdb1e991ece84c651604ea63574eeb30
Sourabh Banik <81009734+Sourabh250@users.noreply.github.com>
2024-09-17 19:46:12 +0900

    Modified cors option

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,18 +12,18 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8001;
 const dbUrl = process.env.DATABASE_URL;
-const corsOrigin = process.env.CORS_ORIGIN || '*';
+// const corsOrigin = process.env.CORS_ORIGIN || '*';
 
 // CORS options to allow credentials and specific origin
-const corsOptions = {
-  origin: corsOrigin,
-  credentials: true, // Allowing cookies and other credentials to be sent
-};
+// const corsOptions = {
+//   origin: corsOrigin,
+//   credentials: true, // Allowing cookies and other credentials to be sent
+// };
 
 app.use(redirect); // Custom middleware for handling redirects
 app.use(express.json()); // Middleware to parse JSON request bodies
 app.use(cookieParser()); // Middleware to parse cookies
-app.use(cors(corsOptions)); // Middleware to enable CORS with options
+app.use(cors()); // Middleware to enable CORS with options
 app.use(helmet()); // Middleware to set various HTTP headers for security
 app.use('/api', routes); // Middleware to use the routes
 app.use(errorHandler); // Custom middleware for error handling
