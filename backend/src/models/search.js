475b5a267df1b0fd341cbc7737e446e4bcf3cdd9
Sourabh Banik <81009734+Sourabh250@users.noreply.github.com>
2024-09-18 01:02:25 +0900

    Modified Model

--- backend/src/models/search.js
+++ backend/src/models/search.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // Schema for searches
 const searchSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true, unique: true },
     searches: [{
         query: { type: String, required: true },
         createdAt: { type: Date, default: Date.now },
